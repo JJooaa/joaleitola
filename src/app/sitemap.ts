5c212811365bf0a944e9f0f33e9888ecbb2f0422
Joa <joa@vertics.co>
2023-11-10 16:03:54 +0100

    Update sitemap URLs and priorities.

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -9,16 +9,34 @@ export default function sitemap(): MetadataRoute.Sitemap {
       priority: 1,
     },
     {
+      url: baseUrl + "/my-services",
+      lastModified: new Date(),
+      priority: 0.9,
+    },
+    {
+      url: baseUrl + "/maximizing-results",
+      lastModified: new Date(),
+      priority: 0.8,
+    },
+    {
       url: baseUrl + "/projects-and-tools",
       lastModified: new Date(),
+      priority: 0.7,
+    },
+    {
+      url: baseUrl + "/contact",
+      lastModified: new Date(),
+      priority: 0.5,
     },
     {
       url: baseUrl + "/projects-and-tools/projects" + "/automotive-industry",
       lastModified: new Date(),
+      priority: 0.5,
     },
     {
       url: baseUrl + "/projects-and-tools/projects" + "/property-management",
       lastModified: new Date(),
+      priority: 0.5,
     },
     {
       url:
@@ -26,23 +44,13 @@ export default function sitemap(): MetadataRoute.Sitemap {
         "/projects-and-tools/projects" +
         "/service-information-system",
       lastModified: new Date(),
+      priority: 0.5,
     },
     {
       url:
         baseUrl + "/projects-and-tools/projects" + "/current-personal-website",
       lastModified: new Date(),
-    },
-    {
-      url: baseUrl + "/my-services",
-      lastModified: new Date(),
-    },
-    {
-      url: baseUrl + "/contact",
-      lastModified: new Date(),
-    },
-    {
-      url: baseUrl + "/maximizing-results",
-      lastModified: new Date(),
+      priority: 0.5,
     },
   ]
 }
